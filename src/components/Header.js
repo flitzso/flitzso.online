6bec140d47caa15cb33845d474e4eb83218d16fe
flitzso <flitzso@hotmail.com>
2024-07-20 21:15:25 -0300

    flitzso

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <div>
+        <h1>ON</h1>
+    </div>
+  );
+};
+
+export default Header;
